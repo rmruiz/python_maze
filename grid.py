d0601b80587811002ed5a28784875915c664180a
Rolando Ruiz <rolando.ruiz@gmail.com>
2026-06-19 11:35:11 -0400

    Updating grid.py and vector.py to make the grid implementation leaner and the vector type more expressive.

--- grid.py
+++ grid.py
@@ -1,15 +1,16 @@
-from copy import deepcopy
-from typing import Any
+from __future__ import annotations
+
+from typing import Tuple, Union
 
 from vector import Vector
 
 class Grid:
     def __init__(self, size: Vector, value: bool = False) -> None:
         self.size: Vector = size
-        self.data: list[list[Any]] = self._create_grid(value)
+        self.data: list[list[bool]] = self._create_grid(value)
 
-    def _create_grid(self, value: bool) -> list[list[Any]]:
-        return [[deepcopy(value) for _ in range(self.size.x)] for _ in range(self.size.y)]
+    def _create_grid(self, value: bool) -> list[list[bool]]:
+        return [[value for _ in range(self.size.x)] for _ in range(self.size.y)]
 
     @property
     def width(self) -> int:
@@ -19,25 +20,25 @@ class Grid:
     def height(self) -> int:
         return self.size.y
 
-    def _pos_to_coords(self, pos):
-        if hasattr(pos, "x") and hasattr(pos, "y"):
+    def _pos_to_coords(self, pos: Union[Vector, Tuple[int, int]]) -> Tuple[int, int]:
+        if isinstance(pos, Vector):
             return pos.x, pos.y
         return pos
 
-    def in_bounds(self, pos):
+    def in_bounds(self, pos: Union[Vector, Tuple[int, int]]) -> bool:
         x, y = self._pos_to_coords(pos)
         return 0 <= x < self.size.x and 0 <= y < self.size.y
 
-    def __getitem__(self, pos):
+    def __getitem__(self, pos: Union[Vector, Tuple[int, int]]) -> bool:
         x, y = self._pos_to_coords(pos)
         return self.data[y][x]
 
-    def __setitem__(self, pos, value):
+    def __setitem__(self, pos: Union[Vector, Tuple[int, int]], value: bool) -> None:
         x, y = self._pos_to_coords(pos)
         self.data[y][x] = value
 
-    def set(self, pos, value):
+    def set(self, pos: Union[Vector, Tuple[int, int]], value: bool) -> None:
         self[pos] = value
 
-    def get(self, pos):
+    def get(self, pos: Union[Vector, Tuple[int, int]]) -> bool:
         return self[pos]
--- vector.py
+++ vector.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
 @dataclass(frozen=True)
@@ -5,18 +7,27 @@ class Vector:
     x: int = 0
     y: int = 0
 
-    def __str__(self):
+    def __repr__(self) -> str:
+        return f"Vector(x={self.x}, y={self.y})"
+
+    def __str__(self) -> str:
         return f"[{self.x}, {self.y}]"
 
-    def north(self):
+    def north(self) -> Vector:
         return Vector(self.x, self.y - 1)
 
-    def south(self):
+    def south(self) -> Vector:
         return Vector(self.x, self.y + 1)
 
-    def east(self):
+    def east(self) -> Vector:
         return Vector(self.x + 1, self.y)
 
-    def west(self):
+    def west(self) -> Vector:
         return Vector(self.x - 1, self.y)
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
           
